295e6fb1457763758d3e08e5691070623d8d46a7
aicha <aicha70tabite@gmail.com>
2023-12-03 21:29:37 +0100

    add session for username

--- admin/index.php
+++ admin/index.php
@@ -13,7 +13,7 @@ if(!isset($_SESSION['id_user'])){
   <meta charset="utf-8">
   <meta content="width=device-width, initial-scale=1.0" name="viewport">
 
-  <title>Dashboard - NiceAdmin Bootstrap Template</title>
+  <title>Dashboard - admin</title>
   <meta content="" name="description">
   <meta content="" name="keywords">
 
@@ -219,7 +219,7 @@ if(!isset($_SESSION['id_user'])){
 
           <a class="nav-link nav-profile d-flex align-items-center pe-0" href="#" data-bs-toggle="dropdown">
             <img src="assets/img/profile-img.jpg" alt="Profile" class="rounded-circle">
-            <span class="d-none d-md-block dropdown-toggle ps-2">K. Anderson</span>
+            <span class="d-none d-md-block dropdown-toggle ps-2"> <?= $_SESSION['usename']?></span>
           </a><!-- End Profile Iamge Icon -->
 
           <ul class="dropdown-menu dropdown-menu-end dropdown-menu-arrow profile">
