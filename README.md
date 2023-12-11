bd39f1c6f738a698eb0ceb6fc9ebe8c86329a056
RabieImghi <rabieaitimghi7@gmail.com>
2023-12-11 11:06:14 +0100

    update

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# plateforme-des-quizzes
+var xhttp = new XMLHttpRequest();
+xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+       document.getElementById("demo").innerHTML = xhttp.responseText;
+    }
+};
+xhttp.open("GET", "filename", true);
+xhttp.send();
--- user/quizze_details.php
+++ user/quizze_details.php
@@ -1,17 +1,14 @@
-
 <?php
 require_once "../connection.php";
 session_start();
-$quizz="ok";
 if(isset( $_SESSION['roleUser']) && $_SESSION['roleUser']=="admin"){
   header('location: ../admin/index.php'); 
+}else if(!isset($_SESSION['roleUser'])){
+    header('location: index.php'); 
 }
+$quizz="ok";
+
 if(isset($_GET['id_cours'])){
-$id_cours=$_GET['id_cours'];
-$select_cours = "SELECT * FROM course NATURAL JOIN quiz WHERE courseID = $id_cours";
-$cours=$conn->query($select_cours);
-$cour=$cours->fetch_assoc();
-$id_quize= $cour['quizID'];  
 ?>
 
 <!DOCTYPE html>
@@ -50,17 +47,23 @@ $id_quize= $cour['quizID'];
         </div>
     </div>
 
-  </div>
-  <!-- ***** Header Area Start ***** -->
-  <?php include('header.php')?>
-  <!-- ***** Header Area End ***** -->
-  <section>
-    <form  method="post" action='scripte_pass_quiz.php'>     
-      <div id="contact" class="contact-us section" >
-          <div class="image" style="width: 100%; height: 200px; overflow: hidden; ">
-            <img src="assets/images/blog-post-01.jpg"  alt=""> 
-          </div>
-          <?php 
+    </div>
+    <?php 
+    include "header.php";
+    $id_cours=$_GET['id_cours'];
+    $select_cours = "SELECT * FROM course NATURAL JOIN quiz WHERE courseID = $id_cours";
+    $cours=$conn->query($select_cours);
+    $cour=$cours->fetch_assoc();
+    $id_quize= $cour['quizID'];  
+  
+  ?>
+    <section>
+        <form method="post" action='scripte_pass_quiz.php'>
+            <div id="contact" class="contact-us section">
+                <div class="image" style="width: 100%; height: 200px; overflow: hidden; ">
+                    <img src="assets/images/blog-post-01.jpg" alt="">
+                </div>
+                <?php 
           $select_question = "SELECT * FROM question WHERE quizID = $id_quize";
           $questions=$conn->query($select_question);
           $i=1;
@@ -68,16 +71,17 @@ $id_quize= $cour['quizID'];
           while($question=$questions->fetch_assoc()){
             $quistion_id=$question['questionID'];
           ?>
-          <div class="container mt-5">
-              <div class="container mt-4 d-flex justify-content-center rounded-1" style="background: #007bff; color: white;">
-                  <h1 class="font-weight-bold" >Question<?=$i++?></h1>
-              </div>
-              <div class="courses pt-4 bg-light font-weight-bold" style="font-size: 20px;">
-                <?=$question["questionText"]?>
-              </div>
-              <div class="container mt-4">
-                  <div class="chois row">
-                      <?php 
+                <div class="container mt-5">
+                    <div class="container mt-4 d-flex justify-content-center rounded-1"
+                        style="background: #007bff; color: white;">
+                        <h1 class="font-weight-bold">Question<?=$i++?></h1>
+                    </div>
+                    <div class="courses pt-4 bg-light font-weight-bold" style="font-size: 20px;">
+                        <?=$question["questionText"]?>
+                    </div>
+                    <div class="container mt-4">
+                        <div class="chois row">
+                            <?php 
                       $select_answer="SELECT * FROM quiz NATURAL JOIN question NATURAL JOIN answer WHERE answer.questionID = $quistion_id";
                       echo "<input type='hidden' name='question_".$reponse_id."_id' value='".$quistion_id."'>";
                       $answers=$conn->query($select_answer);
@@ -86,28 +90,27 @@ $id_quize= $cour['quizID'];
                       if($answers)
                       while($answer=$answers->fetch_assoc()){
                       ?>
-                      <div class="reponse mt-4 col-lg-6 p-3 shadow">
-                          <label for="reponse1" class="p-2" style="font-weight: bold; font-size: x-large;">Réponse <?=$j++?> :</label> <br>
-                          <input type="radio" name="reponse<?=$reponse_id?>" required id="reponse1" value='<?=$answer["answerID"]?>' class="form-check-input">
-                          <span class="ml-2"><?=$answer["answerText"]?></span>
-                      </div>
-                    <?php }
+                            <div class="reponse mt-4 col-lg-6 p-3 shadow">
+                                <label for="reponse1" class="p-2" style="font-weight: bold; font-size: x-large;">Réponse
+                                    <?=$j++?> :</label> <br>
+                                <input type="radio" name="reponse<?=$reponse_id?>" required id="reponse1"
+                                    value='<?=$answer["answerID"]?>' class="form-check-input">
+                                <span class="ml-2"><?=$answer["answerText"]?></span>
+                            </div>
+                            <?php }
                     $reponse_id++; ?>
-                  </div>
-              </div>
-          </div>
-          <?php } ?>
-      </div>
-      <div class="p-5 mt-2" style='width:100%; text-align:center; '>
-        <button type="submit" class="btn btn-success">Success</button>
-      </div>
-      
-    </form>    
-  </section>
+                        </div>
+                    </div>
+                </div>
+                <?php } ?>
+            </div>
+            <div class="p-5 mt-2" style='width:100%; text-align:center; '>
+                <button type="submit" class="btn btn-success">Success</button>
+            </div>
 
+        </form>
+    </section>
 
-
-    <!-- Scripts -->
     <script src="vendor/jquery/jquery.min.js"></script>
     <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
     <script src="assets/js/owl-carousel.js"></script>
@@ -121,4 +124,3 @@ $id_quize= $cour['quizID'];
 <?php   
 }
 ?>
-
